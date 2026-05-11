81e1b8e48387872c255f42eb5a3fe6311adfc037
naroah <givefacegod@gmail.com>
2026-05-11 19:23:45 +0800

    update sdk api

--- src/sdk/api.ts
+++ src/sdk/api.ts
@@ -1,26 +1,41 @@
-import axios from "axios";
 export interface TodoRes {
 	name : string;
 	value : number;
 	todos : string[];
 }
 
-const apiClient = axios.create({
-	baseURL: "https://aws.naroah.top/sloth",
-	timeout: 30000,
-});
+const baseURL = "https://aws.naroah.top/sloth";
+const timeout = 30000;
+
+function post<T>(path : string, data : Record<string, string>) : Promise<T> {
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: `${baseURL}${path}`,
+			method: "POST",
+			data,
+			timeout,
+			header: {
+				"content-type": "application/json",
+			},
+			success: (res) => {
+				const statusCode = res.statusCode ?? 0;
+				statusCode >= 200 && statusCode < 300
+					? resolve(res.data as T)
+					: reject(new Error(`Request failed with status ${statusCode}`));
+			},
+			fail: reject,
+		});
+	});
+}
 
 export async function getSuggest(contents : string) : Promise<string[]> {
-	const res = await apiClient.post("/suggest", { contents });
-	return res.data;
+	return await post<string[]>("/suggest", { contents });
 }
 
 export async function getWrit(contents : string) : Promise<string[]> {
-	const res = await apiClient.post("/writ", { contents });
-	return res.data;
+	return await post<string[]>("/writ", { contents });
 }
 
 export async function getTodo(contents : string) : Promise<TodoRes[]> {
-	const res = await apiClient.post("/todo", { contents });
-	return res.data;
+	return await post<TodoRes[]>("/todo", { contents });
 }
